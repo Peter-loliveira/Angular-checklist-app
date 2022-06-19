429595ae5d2d4b2dc4f6facbc02a0f19a8570756
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2022-06-19 12:13:05 -0300

    Inicio de Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,30 @@
+// import { CategoryComponent } from './category/category.component';
+// import { TestComponent } from './test/test.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CategoryComponent } from './category/category.component';
-import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
+
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material.module';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryComponent,
-    TestComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
+    
+    // componentes não usados
+    // CategoryComponent,
+    // TestComponent,
+    
   ],
   imports: [
     BrowserModule,
